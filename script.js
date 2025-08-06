2a1ea5e65cbf57f0796bfc977eb1603597cc8fb8
vaudemar38-ux <vaudemar38@gmail.com>
2025-08-05 21:51:54 -0300

    Corrige autoplay da música

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
+const musica = document.getElementById("musicaFundo");
+
 function jogar() {
+  tocarMusica();
   alert("Jogo começando (em breve)!");
 }
 
 function opcoes() {
+  tocarMusica();
   alert("Opções indisponíveis no momento.");
 }
 
 function loja() {
+  tocarMusica();
   alert("Loja ainda em construção!");
+}
+
+function tocarMusica() {
+  if (musica.paused) {
+    musica.volume = 0.5; // volume opcional
+    musica.play().catch((err) => {
+      console.log("Falha ao reproduzir a música:", err);
+    });
+  }
 }
