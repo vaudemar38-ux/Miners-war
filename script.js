22bfbee86357e269fa27f6030323258b28938f16
vaudemar38-ux <vaudemar38@gmail.com>
2025-08-05 21:48:41 -0300

    Interface inicial com fundo e som

---
+++ script.js
@@ -0,0 +1,11 @@
+function jogar() {
+  alert("Jogo começando (em breve)!");
+}
+
+function opcoes() {
+  alert("Opções indisponíveis no momento.");
+}
+
+function loja() {
+  alert("Loja ainda em construção!");
+}
